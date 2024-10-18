606c96533a42908537fe0200f6434aca64351905
kamau <githuakamau99@gmail.com>
2024-10-19 02:11:04 +0300

    feat(navbar): make Navbar responsive for different screen sizes

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -3,7 +3,7 @@ import './AppDownload.css'
 import { assets } from '../../assets/assets'
 const AppDownload = () => {
   return (
-    <div className='app-download' id='app-download'>
+    <div className='p' id='app-download'>
       <p>For Better Experience Download <br /> Tomato App </p>
       <div className="app-download-platforms">
         <img src={assets.play_store} alt="" />
