1dc58ed56a4660c46d784c5d97c8d3ff545a2b97
kamau <githuakamau99@gmail.com>
2024-10-19 13:07:10 +0300

    feat(order): add responsive place order form with delivery information and cart totals calculation

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = ({setShowLogin}) => {
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
           <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
-          <div className="dot"></div>
+          <div className={getTotalCartAmount()===0?"":"dot"}></div>
         </div>
         <button onClick={()=>setShowLogin(true)}>sign in</button>
       </div>
