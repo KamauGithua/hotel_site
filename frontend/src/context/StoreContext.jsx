75ce43f0e5824b03b182312691fe4bada5012c43
kamau <githuakamau99@gmail.com>
2024-10-19 12:32:34 +0300

    feat(cart): add functionality to calculate item totals and cart subtotal

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,25 +6,32 @@ export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
 
-    const[cartItems,setCartItems]= useState({});
+    const [cartItems, setCartItems] = useState({});
 
     const addToCart = (itemId) => {
         if (!cartItems[itemId]) {
-            setCartItems((prev)=>({...prev,[itemId]:1}))
+            setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
         }
-        else{
-            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        else {
+            setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
         }
-    } 
-
-    const removeFromCart = (itemId) => {
-        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
     }
 
-    useEffect(()=>{
-        console.log(cartItems);
-        
-    },[cartItems])
+    const removeFromCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+    }
+
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = food_list.find((product) => product._id === item);
+                totalAmount += itemInfo.price * cartItems[item];
+            }
+
+        }
+        return totalAmount;
+    }
 
 
     const contextValue = {
@@ -32,7 +39,8 @@ const StoreContextProvider = (props) => {
         cartItems,
         setCartItems,
         addToCart,
-        removeFromCart
+        removeFromCart,
+        getTotalCartAmount
     }
 
     return (
