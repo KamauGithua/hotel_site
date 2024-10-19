4fad4cc65217045095ef8cc26446ca3c725c610f
kamau <githuakamau99@gmail.com>
2024-10-19 04:14:04 +0300

    feat(auth): create LoginPopup component with toggle between Login and Sign Up

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -1,9 +1,10 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
 import './AppDownload.css'
 import { assets } from '../../assets/assets'
 const AppDownload = () => {
   return (
-    <div className='p' id='app-download'>
+    <div className='app-download' id='app-download'>
       <p>For Better Experience Download <br /> Tomato App </p>
       <div className="app-download-platforms">
         <img src={assets.play_store} alt="" />
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom';
 
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
 
   const [menu,setMenu] = useState("home");
 
@@ -26,7 +26,7 @@ const Navbar = () => {
           <img src={assets.basket_icon} alt="" />
           <div className="dot"></div>
         </div>
-        <button>sign in</button>
+        <button onClick={()=>setShowLogin(true)}>sign in</button>
       </div>
     </div>
   )
