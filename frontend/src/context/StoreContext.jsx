3bab84e8903344973f65fd9c8c178a07daa84a8c
kamau <githuakamau99@gmail.com>
2024-10-26 14:15:32 +0300

    feat: add loadCartData function to fetch and set cart items for authenticated user

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -47,6 +47,12 @@ const StoreContextProvider = (props) => {
         const response = await axios.get(url+"/api/food/list");
         setFoodList(response.data.data)
     }
+
+    const loadCartData = async (token) => {
+        const response = await axios.post(url+"/api/cart/get",{},{headers:{token}});
+        setCartItems(response.data.cartData);
+
+    } 
     
 
     useEffect(()=>{
@@ -55,6 +61,7 @@ const StoreContextProvider = (props) => {
             await fetchFoodList();
             if (localStorage.getItem("token")) {
                 setToken(localStorage.getItem("token"));
+                await loadCartData(localStorage.getItem("token"));
             }
         }
         loadData();
